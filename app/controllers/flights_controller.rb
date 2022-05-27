83b173d12d57681bd2e2cc3b280d967412412f5e
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-27 13:16:23 -0400

    Add departure time selector to form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,6 @@ class FlightsController < ApplicationController
   def index
     @continents = Continent.all.order(:name).includes(:countries)
     @countries = Country.all.order(:name).includes(:airports)
+    @flights = Flight.all.order(:departure)
   end
 end
