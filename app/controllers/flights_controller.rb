88946eff2a34c826516f87c9c6c79667949188ac
Nicolás Órdenes <nicordenes@gmail.com>
2022-06-17 17:31:58 -0400

    Add countries lists based on available flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all.order(:departure).includes(:departure_airport)
-    @countries = @flights.map(&:departure_airport)
-                         .map(&:country).uniq
-                         .sort { |a, b| a.name <=> b.name }
+    @departure_countries = @flights.map(&:departure_airport).map(&:country).uniq.sort { |a, b| a.name <=> b.name }
+    @arrival_countries = @flights.map(&:destination_airport).map(&:country).uniq.sort { |a, b| a.name <=> b.name }
   end
 end
