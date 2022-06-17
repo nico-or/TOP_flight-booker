15170f8fcb145f6fce2c86e414092cb5eaf30784
Nicolás Órdenes <nicordenes@gmail.com>
2022-06-17 17:35:32 -0400

    Add flight selection form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,18 @@ class FlightsController < ApplicationController
     @flights = Flight.all.order(:departure).includes(:departure_airport)
     @departure_countries = @flights.map(&:departure_airport).map(&:country).uniq.sort { |a, b| a.name <=> b.name }
     @arrival_countries = @flights.map(&:destination_airport).map(&:country).uniq.sort { |a, b| a.name <=> b.name }
+
+    unless search_params.empty?
+      @selection = []
+      @selection += Airport.find_by_code(search_params[:departure_airport]).outgoing_flights.records
+      @selection += Airport.find_by_code(search_params[:arrival_airport]).incoming_flights.records
+      @selection << Flight.find(search_params[:time])
+    end
+  end
+
+  private
+
+  def search_params
+    params.permit(:departure_airport, :arrival_airport, :time)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -10,4 +10,16 @@ class Flight < ApplicationRecord
   def strf_departure_dmy
     departure.strftime('%d/%m/%Y')
   end
+
+  def radio_label
+    departure_airport_name = departure_airport.name
+    destination_airport_name = destination_airport.name
+    departure_country_name = Country.find(departure_airport.country_id).name
+    destination_country_name = Country.find(destination_airport.country_id).name
+
+    departure_string = "#{departure_airport_name} (#{departure_country_name})"
+    destination_string = "#{destination_airport_name} (#{destination_country_name})"
+
+    "#{departure_string} → #{destination_string} @#{departure}"
+  end
 end
