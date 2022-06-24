0da2396ee9e70ee53b74484d84a6de9aab643a43
Nicolás Órdenes <nicordenes@gmail.com>
2022-06-24 17:48:58 -0400

    Add nested attributes for passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,5 +2,13 @@ class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
     @booking = @flight.bookings.build
+    params[:num_tickets].to_i.times { @booking.passengers.build }
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id,
+      passengers_attributes: [:name, :email])
   end
 end
