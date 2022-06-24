893acab3736189cd694ad2c8583d7dd171c03138
Nicolás Órdenes <nicordenes@gmail.com>
2022-06-24 18:31:05 -0400

    Add Passenger model validations

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,7 @@
 class Passenger < ApplicationRecord
   has_and_belongs_to_many :bookings
   has_many :flights, through: :bookings
+
+  validates_presence_of :name, :email
+  validates_uniqueness_of :name, :email
 end
